35b64d5f3f97f30d4fe5814957e5a369755bba46
Pavel Fayustov <p.fayustov@kck.ru>
2021-08-07 19:18:15 +0300

    Добавил сортировку куплено\не куплено

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let $list = document.getElementById('list');
 let $filter = document.getElementById('filter');
 let $sorting = document.getElementById('sortingType');
 let $checkGood = null;
+let $amount = document.querySelector('.length');
 
 const objPropsMap = {
   name: 'Наименование товара',
@@ -16,11 +17,18 @@ const objPropsMap = {
   purchaseDate: 'Дата покупки',
 };
 
+function countGoods(array) {
+  $amount.textContent = `Количество товаров: ${array.length}`;
+}
+
 function renderItem(item) {
   let $el = document.createElement('li');
   let $internalList = document.createElement('ul');
   for (let prop in item) {
     if (item[prop] !== '') {
+      if (prop === 'isBought') {
+        continue;
+      }
       let LabelFromMap = objPropsMap[prop];
       let $internalListItem = document.createElement('li');
       $internalListItem.textContent = LabelFromMap + ' ' + item[prop];
@@ -29,14 +37,18 @@ function renderItem(item) {
   }
   $el.append($internalList);
   let $checkbox = document.createElement('input');
-  $checkbox.setAttribute('type', 'checkbox');
+  $checkbox.setAttribute('type', 'checkbox');    
+  $checkbox.setAttribute('data-name', item.name);
   $checkbox.classList.add("check-good");
+
+  if (item.isBought) {
+    $el.classList.add("item-purchased");
+    $checkbox.setAttribute('checked', 'checked');
+  }
 
   $el.prepend($checkbox);
   $list.append($el);
 
-  let $amount = document.querySelector('.length');
-  $amount.textContent = `Количество товаров: ${goodsList.length}`;
   $name.value = '';
   $quantity.value = '';
   $price.value = '';
@@ -49,29 +61,32 @@ function renderList(array) {
   array.forEach((el) => {
     renderItem(el);
   })
+  countGoods(array);
 };
 
 
 document.getElementById('add').addEventListener('click', function () {
-  // сделать запрет на добавление только ПОЛНОГО дубликата (когда все поля в существующем объекте абсолютно равны)
   if ($name.value !== '') {
     const obj = {
       name: $name.value,
       quantity: $quantity.value,
       price: $price.value,
       purchaseDate: $purchaseDate.value,
+      isBought: false,
     };
 
     if (goodsList.length === 0) {
       goodsList.push(obj);
       renderItem(obj);
+      countGoods(goodsList);
     } else {
 
-        if (goodsList.find((item) => JSON.stringify(item) === JSON.stringify(obj))) {
+        if (goodsList.find((item) => item.name === obj.name)) {
         alert('Нельзя добавить существующий товар');
       } else {
         goodsList.push(obj);
         renderItem(obj);
+        countGoods(goodsList);
       }
     }
   } else {
@@ -83,6 +98,7 @@ $filter.addEventListener('input', function () {
   const regexpOrder = new RegExp(this.value, 'i');
   filteredList = goodsList.filter((el) => regexpOrder.test(el.name));
   renderList(filteredList);
+  countGoods(filteredList);
 });
 
 //sorting
@@ -105,14 +121,34 @@ $sorting.addEventListener('change', function () {
 
 
 function updateCheckboxList() {
-  $checkGood = document.querySelectorAll('.check-good');
+   $checkGood = document.querySelectorAll('.check-good');
   $checkGood.forEach((el) => {
     el.addEventListener('click', function () {
+      let dataValue = event.target.getAttribute("data-name");
+      let foundObj = goodsList.find(item => item.name === dataValue);
       if (this.checked) {
-        el.nextSibling.classList.add("item-purchased");
+        foundObj.isBought = true;
+        renderList(goodsList);
       } else {
-        el.nextSibling.classList.remove("item-purchased");
+        foundObj.isBought = false;
+        renderList(goodsList);
       }
     });
   });
 }
+
+
+document.getElementById('all-goods').addEventListener('click', function () {
+    renderList(goodsList);
+});
+
+document.getElementById('bought-goods').addEventListener('click', function () {
+  let boughtGoods = goodsList.filter(el => el.isBought === true);
+  renderList(boughtGoods);
+});
+
+document.getElementById('planned-goods').addEventListener('click', function () {
+  let plannedGoods = goodsList.filter(el => el.isBought === false);
+  renderList(plannedGoods);
+});
+
